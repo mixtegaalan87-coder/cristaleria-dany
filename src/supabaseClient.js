b61bada8a3d3d4e5b9472a1ee463828a12990498
Alan <alpastortacos11@gmail.com>
2026-03-17 22:12:02 -0600

    Se actualizo la urlKey para conectar la base de datos-lo sube ala

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Usando los datos de tus capturas
 const supabaseUrl = 'https://oowwqmrrkhtfltgdepjl.supabase.co';
-const supabaseKey = 'sb_publishable_20WmGS43be-_pEmvzcSOaw_AAVPuNfXW98iBwG';
+const supabaseKey = 'sb_publishable_20WmGS43be-_pEmvzcS0aw_AAVPuQqQ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
